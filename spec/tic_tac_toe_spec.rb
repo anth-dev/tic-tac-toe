8d073e97c1a06cb7f9ff75197f53ac1ce49f9cc0
Anthony Giles <omgliekpie@gmail.com>
2021-05-27 22:44:38 -0600

    first two tests working

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -4,37 +4,29 @@ require_relative '../lib/tic_tac_toe'
 
 describe GameBoard do
   describe '#check_for_win' do
+    subject(:board) { described_class.new }
 
     context 'vertical win' do
-      subject(:vertical_win) { described_class.new }
-
-      before do
-        vertical_win.a1 = Token.new('X')
-        vertical_win.a2 = Token.new('X')
-        vertical_win.a3 = Token.new('X')
-      end
 
       it 'should declare a winner' do
-        allow(vertical_win).to receive(:display_board)
-        expect(vertical_win).to receive(:puts).with('X is the winner!')
-        vertical_win.check_for_win
+        board.a1 = Token.new('X')
+        board.a2 = Token.new('X')
+        board.a3 = Token.new('X')
+        expect(board).to receive(:declare_winner).with(board.a1.owner)
+        board.check_for_win
       end
     end
 
     context 'diagonal win' do
-      subject(:diagonal_win) { described_class.new }
-
-      before do
-        diagonal_win.a1 = Token.new('X')
-        diagonal_win.b2 = Token.new('X')
-        diagonal_win.c3 = Token.new('X')
-      end
 
       it 'should declare a winner' do
-        allow(diagonal_win).to receive(:display_board)
-        expect(diagonal_win).to receive(:puts).with('X is the winner!')
-        diagonal_win.check_for_win
+        board.a1 = Token.new('X')
+        board.b2 = Token.new('X')
+        board.c3 = Token.new('X')
+        expect(board).to receive(:declare_winner).with(board.a1.owner)
+        board.check_for_win
       end
+    
     end
 
   end
