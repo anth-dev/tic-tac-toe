e19b3d55964c9d1dbbaa16ccaf4a6f6fb0212cde
Anthony Giles <omgliekpie@gmail.com>
2021-08-07 11:00:52 -0600

    change the doubles to instance_doubles

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,7 +15,7 @@ describe Board do
 
     context 'when a left vertical win condition has been met' do 
       subject(:board_with_left_vertical_win) { described_class.new( [[test_token_for_left_vertical_win, test_token_for_left_vertical_win, test_token_for_left_vertical_win],[nil, nil, nil],[nil, nil, nil]]) }
-      let(:test_token_for_left_vertical_win) { double("Token", :owner => 'X') }
+      let(:test_token_for_left_vertical_win) { instance_double("Token", :owner => 'X') }
 
       it 'should return true' do
         expect(board_with_left_vertical_win).to be_game_won
@@ -24,7 +24,7 @@ describe Board do
 
     context 'when a middle vertical win condition has been met' do
       subject(:board_with_middle_vertical_win) { described_class.new( [[nil, nil, nil], [test_token_for_middle_vertical_win, test_token_for_middle_vertical_win, test_token_for_middle_vertical_win], [nil, nil, nil]] ) }
-      let(:test_token_for_middle_vertical_win) { double("Token", :owner => 'O') }
+      let(:test_token_for_middle_vertical_win) { instance_double("Token", :owner => 'O') }
 
       it 'should return true' do
         expect(board_with_middle_vertical_win).to be_game_won
@@ -33,7 +33,7 @@ describe Board do
 
     context 'when a right vertical win condition has been met' do
       subject(:board_with_right_vertical_win) { described_class.new( [[nil, nil, nil], [nil, nil, nil], [test_token_for_right_vertical_win, test_token_for_right_vertical_win, test_token_for_right_vertical_win]] ) }
-      let(:test_token_for_right_vertical_win) { double("Token", :owner => 'O') }
+      let(:test_token_for_right_vertical_win) { instance_double("Token", :owner => 'O') }
 
       it 'should return true' do
         expect(board_with_right_vertical_win).to be_game_won
@@ -42,7 +42,7 @@ describe Board do
 
     context 'when a top horizontal win condition has been met' do
       subject(:board_with_top_horizontal_win) { described_class.new( [[nil, nil, test_token_for_top_horizonal_win], [nil, nil, test_token_for_top_horizonal_win], [nil, nil, test_token_for_top_horizonal_win]] ) }
-      let(:test_token_for_top_horizonal_win) { double("Token", :owner => 'X') }
+      let(:test_token_for_top_horizonal_win) { instance_double("Token", :owner => 'X') }
 
       it 'should return true' do
         expect(board_with_top_horizontal_win).to be_game_won
@@ -51,7 +51,7 @@ describe Board do
 
     context 'when a middle horizontal win condition has been met' do
       subject(:board_with_middle_horizontal_win) { described_class.new( [[nil, test_token_for_middle_horizontal_win, nil], [nil, test_token_for_middle_horizontal_win, nil], [nil, test_token_for_middle_horizontal_win, nil]] ) }
-      let(:test_token_for_middle_horizontal_win) { double("Token", :owner => 'O') }
+      let(:test_token_for_middle_horizontal_win) { instance_double("Token", :owner => 'O') }
 
       it 'should return true' do
         expect(board_with_middle_horizontal_win).to be_game_won
@@ -60,7 +60,7 @@ describe Board do
 
     context 'when a bottom horizontal win condition has been met' do
       subject(:board_with_bottom_horizontal_win) { described_class.new( [[test_token_for_bottom_horizontal_win, nil, nil], [test_token_for_bottom_horizontal_win, nil, nil], [test_token_for_bottom_horizontal_win, nil, nil]] ) }
-      let(:test_token_for_bottom_horizontal_win) { double("Token", :owner => 'O') }
+      let(:test_token_for_bottom_horizontal_win) { instance_double("Token", :owner => 'O') }
 
       it 'should return true' do
         expect(board_with_bottom_horizontal_win).to be_game_won
@@ -69,7 +69,7 @@ describe Board do
 
     context 'when a top left to bottom right diagonal win condition has been met' do
       subject(:board_with_top_left_to_bottom_right_win) { described_class.new ( [[nil, nil, test_token_for_top_left_to_bottom_right_win], [nil, test_token_for_top_left_to_bottom_right_win, nil],[test_token_for_top_left_to_bottom_right_win, nil, nil]] ) }
-      let(:test_token_for_top_left_to_bottom_right_win) { double("Token", :owner => 'X') }
+      let(:test_token_for_top_left_to_bottom_right_win) { instance_double("Token", :owner => 'X') }
 
       it 'should return true' do
         expect(board_with_top_left_to_bottom_right_win).to be_game_won
@@ -78,7 +78,7 @@ describe Board do
 
     context 'when a bottom left to top right diagonal win condition has been met' do
       subject(:board_with_bottom_left_to_top_right_win) { described_class.new( [[test_token_for_bottom_left_to_top_right_win, nil, nil], [nil, test_token_for_bottom_left_to_top_right_win, nil], [nil, nil, test_token_for_bottom_left_to_top_right_win]] ) }
-      let(:test_token_for_bottom_left_to_top_right_win) { double("Token", :owner => 'O') }
+      let(:test_token_for_bottom_left_to_top_right_win) { instance_double("Token", :owner => 'O') }
 
       it 'should return true' do
         expect(board_with_bottom_left_to_top_right_win).to be_game_won
@@ -90,7 +90,7 @@ describe Board do
 
     context 'when X has a top horizontal win' do
       subject(:board_with_top_horizontal_win) { described_class.new( [[nil, nil, test_token_for_top_horizonal_win], [nil, nil, test_token_for_top_horizonal_win], [nil, nil, test_token_for_top_horizonal_win]] ) }
-      let(:test_token_for_top_horizonal_win) { double("Token", :owner => 'X') }
+      let(:test_token_for_top_horizonal_win) { instance_double("Token", :owner => 'X') }
 
       it 'should return X' do
         expect(board_with_top_horizontal_win.return_winner).to eq('X')
@@ -100,7 +100,7 @@ describe Board do
 
     context 'when developers have a middle vertical win' do
       subject(:board_with_middle_vertical_win) { described_class.new( [[nil, nil, nil], [test_token_for_middle_vertical_win, test_token_for_middle_vertical_win, test_token_for_middle_vertical_win], [nil, nil, nil]] ) }
-      let(:test_token_for_middle_vertical_win) { double("Token", :owner => 'developers') }
+      let(:test_token_for_middle_vertical_win) { instance_double("Token", :owner => 'developers') }
 
       it 'should return developers' do
         expect(board_with_middle_vertical_win.return_winner).to eq('developers')
@@ -109,7 +109,7 @@ describe Board do
 
     context 'when O has a diagonal win' do
       subject(:board_with_bottom_left_to_top_right_win) { described_class.new( [[test_token_for_bottom_left_to_top_right_win, nil, nil], [nil, test_token_for_bottom_left_to_top_right_win, nil], [nil, nil, test_token_for_bottom_left_to_top_right_win]] ) }
-      let(:test_token_for_bottom_left_to_top_right_win) { double("Token", :owner => 'O') }
+      let(:test_token_for_bottom_left_to_top_right_win) { instance_double("Token", :owner => 'O') }
 
       it 'should return O' do
         expect(board_with_bottom_left_to_top_right_win.return_winner).to eq('O')
@@ -129,8 +129,8 @@ describe Board do
 
     context 'when there is a draw' do
       subject(:board_with_a_draw) { described_class.new( [[test_x_token_for_draw, test_o_token_for_draw, test_x_token_for_draw], [test_o_token_for_draw, test_o_token_for_draw, test_x_token_for_draw], [test_x_token_for_draw, test_x_token_for_draw, test_o_token_for_draw]] ) }
-      let(:test_x_token_for_draw) { double("Token", :owner => 'X') }
-      let(:test_o_token_for_draw) { double("Token", :owner => 'O') }
+      let(:test_x_token_for_draw) { instance_double("Token", :owner => 'X') }
+      let(:test_o_token_for_draw) { instance_double("Token", :owner => 'O') }
 
       it 'should return true' do
         expect(board_with_a_draw).to be_game_draw
@@ -139,8 +139,8 @@ describe Board do
 
     context 'when there is no draw' do
       subject(:board_with_no_draw) { described_class.new( [[test_x_token_for_draw, test_x_token_for_draw, test_x_token_for_draw], [test_o_token_for_draw, test_x_token_for_draw, test_o_token_for_draw], [test_o_token_for_draw, test_x_token_for_draw, test_o_token_for_draw]] ) }
-      let(:test_x_token_for_draw) { double("Token", :owner => 'X') }
-      let(:test_o_token_for_draw) { double("Token", :owner => 'O') }
+      let(:test_x_token_for_draw) { instance_double("Token", :owner => 'X') }
+      let(:test_o_token_for_draw) { instance_double("Token", :owner => 'O') }
 
       it 'should return false' do
         expect(board_with_no_draw).not_to be_game_draw
